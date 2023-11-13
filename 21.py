d0f1cf2a8693e378c467f55727f8511df9bd3e01
Jep-adano <qjbadano@tip.edu.ph>
2023-11-13 05:16:12 +0000

    21

---
+++ 21.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+heart = [
+    {
+        "heart-id": "0",
+        "date": "13/11/2023",
+        "heart-rate": "110bpm"
+    },
+    {
+        "heart-id": "3",
+        "date": "14/12/2024",
+<<<<<<< HEAD
+        "heart-rate": "140bpm"
+=======
+        "heart-rate": "180bpm"
+>>>>>>> f6bca4c9a9a7116d20f318c866426ffd8626fb9b
+    }
+
+]
+
+@app.route('/21', methods=['GET'])
+def get_heart():
+    return jsonify(heart)
+
+@app.route('/21', methods=['POST'])
+def add_heart():
+    new_heart = request.get_json()
+    heart.append(new_heart)
+    return {'heart-id':len(new_heart)}, 200
+
+
+@app.route('/21/<int:index>', methods=['DELETE'])
+def delete_heart(index):
+    heart.pop =(index)
+    return 'Heart rate deleted', 200
+
+if __name__ == "__main__":
+    app.run()
